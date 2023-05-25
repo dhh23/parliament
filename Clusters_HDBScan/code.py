b4c5e9161bcf8cebcfa427ec574b38f6bef99631
Vadym4 <86015479+Vadym4@users.noreply.github.com>
2023-05-25 16:01:57 +0200

    Update code.py

--- Clusters_HDBScan/code.py
+++ Clusters_HDBScan/code.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 import numpy as np
 import time
 import csv
+import random
 
 print("starting")
 # Load embeddings from pickle file
@@ -12,12 +13,15 @@ with open("ParlaMint_GB_commons_aggregated_embeddings.pkl", "rb") as openfile:
     #dictionary speech_id -> embedings
     embeddings = pickle.load(openfile)
 
-embeddings_array = np.array(list(embeddings.values()))
+#print({k: embeddings[k] for k in list(embeddings)[:2]})
+
+#embeddings_array = np.array(list(embeddings.values()))
 
 #speech_id, cluster_id
 
 # First part: not aggregated
 clusterer = hdbscan.HDBSCAN(min_cluster_size=2)
+
 
 '''
 Parameters
@@ -31,14 +35,27 @@ min_samples (number of neighbours which will be considered):
 cluster_selection_epsilon (radius of neighbours which will be considered) -> leave at default 
 '''
 subset_size = 2000
-subset_array = embeddings_array[:subset_size]
+#subset_array = embeddings_array[:subset_size]
+
+embeddings_list = list(embeddings.items())
+
+# Randomly sample 2000 items from the list
+embeddings_items = random.sample(embeddings_list, k=subset_size)
+
+# Create a new dictionary with the subset of items
+embeddings_subdic = dict(embeddings_items)
+
+
 
 #subset_indices = np.random.choice(embeddings_array.shape[0], subset_size, replace=False)
 #subset_embeddings = embeddings_array[subset_indices]
 
 print("starting clustering")
 
-cluster_labels = clusterer.fit_predict(subset_array)
+cluster_labels = clusterer.fit_predict(list(embeddings_subdic.values()))
+
+keys_list = list(embeddings.keys())
+print(cluster_labels)
 
 #now write into csv
 f = open('minclustersize_1.csv', 'w',newline='')
@@ -46,8 +63,14 @@ writer = csv.writer(f)
 fieldnames = ['ID', 'cluster_ID']
 writer = csv.DictWriter(f, fieldnames=fieldnames)
 writer.writeheader()
-    
+
+
 for i in range(2000):
-    writer.writerow({'ID': str(i+1), 'cluster_ID': cluster_labels[i]})
+    writer.writerow({'ID': keys_list[i], 'cluster_ID': cluster_labels[i]})
 
 f.close()
+
+
+
+#clustering = clusterer.fit(subset_embeddings)
+#print("done")
